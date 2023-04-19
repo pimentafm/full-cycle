e30519d8362da8d019776d1b6f60c3473aaf61ed
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2023-04-19 13:07:27 -0300

    Add new project

--- docker/desafio-nginx/node/index.js
+++ docker/desafio-nginx/node/index.js
@@ -1,58 +1,34 @@
-const express = require('express');
-const axios = require('axios').default;
-const mysql = require('mysql');
-
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const faker = require('faker')
+const app = express()
+const port = process.env.APP_PORT || 3000
 
 const config = {
-  host: 'db',
+  host: 'app',
   user: 'root',
-  password: 'password',
-  database: 'nodedb',
-};
+  password: 'root',
+  database: 'db',
+}
+
+const mysql = require('mysql')
+
+const connection = mysql.createConnection(config)
 
 app.get('/', (req, res) => {
-  insertPeopleName(res);
-});
+  const name = faker.name.findName()
 
-app.listen(PORT, () => {
-  console.log('STARTED AT ' + PORT);
-});
+  connection.query(`INSERT INTO people (nome) VALUES ('${name}')`)
 
-async function getPersonName() {
-  const RANDOM = Math.floor(Math.random() * 10);
-  const response = await axios.get('https://swapi.dev/api/people');
-  personName = response.data.results;
-  return personName[RANDOM].name;
-}
+  connection.query(`SELECT nome FROM people`, (error, results, fields) => {
+    res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ol>
+        ${!!results.length ? results.map(el => `<li>${el.nome}</li>`).join('') : ''}
+      </ol>
+    `)
+  })
+})
 
-async function insertPeopleName(res) {
-  const name = await getPersonName();
-  const connection = mysql.createConnection(config);
-  const sql = `INSERT INTO people(name) values('${name}')`;
-    
-  connection.query(sql);
-  console.log(`${name} inserido no banco!`);    
-  getPeople(res, connection);
-}
-
-function getPeople(res, connection) {    
-  const sql = `SELECT id, name FROM people`;  
-  
-  connection.query(sql, (error, results, fields) => {
-    if (error) {
-      throw error
-    };
-    
-    let table = '<table>';
-    table += '<tr><th>#</th><th>Name</th></tr>';
-    for(let people of results) {      
-      table += `<tr><td>${people.id}</td><td>${people.name}</td></tr>`;
-    }
-
-    table += '</table>';    
-    res.send('<h1>Full Cycle Rocks!</h1>' + table);    
-  });   
-  connection.end();
-}
+app.listen(port, () => {
+  console.log('Up on:', port);
+})
